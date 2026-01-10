54a03f1ffe8586db30c31bbfc16a0eef5d60bedb
Anas Sarwar <anassarwar14@gmail.com>
2026-01-11 01:40:19 +0500

    hi

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -14,8 +14,8 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      "fixed ixed bottom-[40px] left-1/2 -translate-x-1/2 top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px]",
-      // "fixed bottom-[40px] left-1/2 -translate-x-1/2 z-[100] flex flex-col-reverse p-4 max-w-[420px] w-full",
+      // "fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px]",
+        "fixed bottom-[100px] left-1/2 -translate-x-1/2 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:flex-col md:max-w-[420px]",
       className,
     )}
     {...props}
